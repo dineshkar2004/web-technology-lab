c6b5b635f6f610ceb5de32a44505d24e480ca6ff
dineshkar2004 <dineshkarthikk2020@gmail.com>
2024-11-26 07:32:23 +0530

    [all experiment here]

---
+++ 5th experiment/script.js
@@ -0,0 +1,12 @@
+
+function pro(nums){
+    num1=document.getElementById("one").value;
+    num2=document.getElementById("two").value;
+    document.getElementById("result").innerHTML=num1*num2;
+}
+
+function div(nums){
+    num1=document.getElementById("one").value;
+    num2=document.getElementById("two").value;
+    document.getElementById("result").innerHTML=num1/num2;
+}
